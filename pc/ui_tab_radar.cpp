0eeeadb1f85b0f9ad5122498a6e1ad493d822607
Christian M. Netter <chris@chrisnetter.com>
2015-12-29 21:36:45 -0500

    Cleaned up and improved the occupancy grid UI code

--- pc/ui_tab_radar.cpp
+++ pc/ui_tab_radar.cpp
@@ -3,10 +3,11 @@
 #include <stdio.h>
 #include <windows.h>
 
-#define grid_size 25
-#define cell_size (25.4/0.25)
+#define grid_size 200
+#define cell_size (25.4/1)
 #define DRAW_ROBOT 1
 #define IMMEDIATE_UPDATE 1
+#define DRAW_GRID 0
 
 #if IMMEDIATE_UPDATE
 #define GRID_VALUE_INC 5
@@ -104,8 +105,16 @@ namespace robot_ui
 {
 	void f1::update_grid(float d, float x0, float y0, float t0,float x1, float y1, float t1)
 	{
-		float t2,x2,y2,x3,y3,x4,y4;
-		static float lx3=0,ly3=0;
+		/*
+			d:			distance in mm;  i.e. the sensor reading
+			x0,y0,t0:	robot position & heading (x, y, theta)
+			x1,y1,t1:	sensor position & direction, relative to the robot's center 
+						(x1 and y1 are 0 for Lidar, but non-zero for IR sensors positioned around the robot base perimeter)
+
+		*/
+		float x2,y2,x3,y3,x4,y4;
+		int ix3,iy3, ix4, iy4;
+		volatile static int lx3=0,ly3=0;
 		int i = history_index;
 		int inc, dec, max;
 		
@@ -113,46 +122,50 @@ namespace robot_ui
 		dec = Convert::ToInt32(radar_textBox_dec->Text);
 		max = Convert::ToInt32(radar_textBox_max->Text);
 
-		//d=((float)inputs_history[i].ir[ir_sensor_index]/10.0)*cell_size;
-		//d=inputs_history[i].lidar.samples[ir_sensor_index]*1000;
-
-		//i=i+offset;
-
-		t2= t0; //inputs_history[i].theta; 
+		//x2,y2 are the coordinates of the obstacle, relaive to the sensor's origin
 		x2=x1+d*cos(t1);
 		y2=y1+d*sin(t1);
-		x3=x2*cos(t2)-y2*sin(t2);
-		y3=x2*sin(t2)+y2*cos(t2);
-		x3+=x0; //inputs_history[i].x;
-		y3+=y0; //inputs_history[i].y;
-		x4=x1*cos(t2)-y1*sin(t2);
-		y4=x1*sin(t2)+y1*cos(t2);
-		x4+=x0; //inputs_history[i].x;
-		y4+=y0; //inputs_history[i].y;
 
-		//if(d<750) 
-		if((d>0) && (d<5000))
+		//x3,y3 are the absolute coordinates of the obstacle
+		x3=x2*cos(t0)-y2*sin(t0);
+		y3=x2*sin(t0)+y2*cos(t0);
+		x3+=x0; 
+		y3+=y0; 
+		ix3 = (int)(x3/cell_size);
+		iy3 = (int)(y3/cell_size);
+
+
+		//x4,y4 are the absolute coordinates of the sensor;  only needed so that grid cells that are known to be empty can be marked empty
+		x4=x1*cos(t0)-y1*sin(t0);
+		y4=x1*sin(t0)+y1*cos(t0);
+		x4+=x0; 
+		y4+=y0; 
+		ix4 = (int)(x4/cell_size);
+		iy4 = (int)(y4/cell_size);
+
+
+		//only updated the grid if the sensor reading is valid
+		if((d>150) && (d<6000))
 		{
-			plotLine((int)(x4/cell_size),(int)(y4/cell_size), (int)(x3/cell_size),(int)(y3/cell_size) , dec);
+			plotLine(ix4,iy4, ix3,iy3, dec);
 
-			if ((x3 / cell_size>0) && (y3 / cell_size > 0) && (x3 / cell_size < grid_size) && (y3 / cell_size < grid_size))
+			if( (ix3>0) && (iy3>0) && (ix3<grid_size) && (iy3<grid_size) )
 			{
+				grid[ix3][iy3].value += inc;
+				if(grid[ix3][iy3].value > max) grid[ix3][iy3].value = max;
+				grid[ix3][iy3].changed = 1;
 
-				grid[(int)(x3 / cell_size)][(int)(y3 / cell_size)].value += inc;
-				if (grid[(int)(x3 / cell_size)][(int)(y3 / cell_size)].value > max) grid[(int)(x3 / cell_size)][(int)(y3 / cell_size)].value = max;
-				grid[(int)(x3 / cell_size)][(int)(y3 / cell_size)].changed = 1;
-
-				if ((abs(x3 - lx3) < 150) && (abs(y3 - ly3) < 150))
+				if ((abs(ix3 - lx3) < 20) && (abs(iy3 - ly3) < 20))
 				{
 					if (radar_checkBox_use_lines->Checked)
 					{
-						plotLine((int)(lx3 / cell_size), (int)(ly3 / cell_size), (int)(x3 / cell_size), (int)(y3 / cell_size), inc);
+						plotLine(lx3, ly3, ix3, iy3, inc);
 					}
 				}
 			}
+			lx3=ix3;
+			ly3=iy3;
 		}
-		lx3=x3;
-		ly3=y3;
 	}
 	/******************************************************************************************************************************************
 	*
@@ -184,24 +197,12 @@ namespace robot_ui
 			//printf("-");
 		}
 
-		/*
-		for(x=g->VisibleClipBounds.X; x<g->VisibleClipBounds.X + g->VisibleClipBounds.Width; x+=10)
-		{
-		for(y=g->VisibleClipBounds.Y; y<g->VisibleClipBounds.Y + g->VisibleClipBounds.Height; y+=10)
-		{
-		//g->DrawRectangle(p2,x,y,10,10);
-		g->FillRectangle(b,x+2,y+2,7,7);
-		count++;
-		}
-		}
-		*/
-		//log_printf("grid x,y,total,w,h=%d,%d,%d,%f,%f\n",x_count,y_count,count,g->VisibleClipBounds.Width,g->VisibleClipBounds.Height);
 	}
 
 	void f1::ShowRadar(int ex, int ey)
 	{
 		int x,y,i,j;
-		int increment;
+		float increment;
 		const int pensize=10;
 		Brush^ b0 =  gcnew SolidBrush(Color::White);
 		Brush^ b1 =  gcnew SolidBrush(Color::Black);
@@ -215,7 +216,7 @@ namespace robot_ui
 		Pen^ p4 = gcnew Pen(Color::Blue, pensize);
 
 		increment = (g->VisibleClipBounds.Height / grid_size);
-#if 1
+#if 0
 		for(x=0;x<grid_size;x++)
 		{
 			for(y=0;y<grid_size;y++)
@@ -247,23 +248,23 @@ namespace robot_ui
 			for(y=0;y<grid_size;y++)
 			{
 				if(grid[x][y].value>0) bitmap[x][y]=0; else bitmap[x][y]=0xffffffffUL;
+				if(grid[x][y].value>0) grid[x][y].value--;
 			}
 		}
 
 		System::IntPtr ptr(bitmap);
-		System::Drawing::Bitmap^ bm  = gcnew System::Drawing::Bitmap(grid_size,grid_size,400,System::Drawing::Imaging::PixelFormat::Format32bppRgb,ptr);
+		System::Drawing::Bitmap^ bm  = gcnew System::Drawing::Bitmap(grid_size,grid_size,grid_size*4,System::Drawing::Imaging::PixelFormat::Format32bppRgb,ptr);
+		bm->RotateFlip(System::Drawing::RotateFlipType::Rotate270FlipNone);
 		System::Drawing::RectangleF rect(0,0,pictureBox1->Width,pictureBox1->Height);
 		g->DrawImage(bm,rect);	
 
 #endif
 #if DRAW_ROBOT
-		//g->FillEllipse(b2, (int)((inputs_history[i].x-100)/cell_size)*increment, (int)g->VisibleClipBounds.Height - (int)((inputs_history[i].y+100)/cell_size)*(increment) ,(int)(200/cell_size)*increment, (int)(200/cell_size)*increment);
-		//if(ex>2)
-		{
-			g->DrawEllipse(p2, ex, ey,(int)(20/cell_size)*increment, (int)(20/cell_size)*increment);
-		}
+		g->DrawEllipse(p2, ex, ey,(int)((20.0f/cell_size)*increment), (int)((20.0f/cell_size)*increment));
 #endif
 	}
+
+
 
 	void f1::UpdateRadar(float a, int b)
 	{
@@ -271,56 +272,49 @@ namespace robot_ui
 		static float x=0,y=0,theta=0;
 		float x1,y1,x2,y2,rx,ry;
 		int scan_completed=0;
-		DWORD t1,t2,t3,count=0;
-		//int offset = radar_trackBar1->Value;
+		static DWORD t1,t2,t3,count=0;
+		float angle = Convert::ToSingle(radar_textBox_angle->Text);
+		int j;
+		t_inputs h,h2;
 
 #ifdef USE_LIDAR
 
-		//DrawGrid(g);
-		if(history_index - i > 2000) i=history_index-2000;
+#if DRAW_GRID
+		DrawGrid(g);
+#endif
+
+		if(history_index - i > 200) i=history_index-200;
 
 		t1 = timeGetTime();
 
-		//for(i=history_index-360; i<=history_index;i++)
 		scan_completed=0;
 		while( (i<(history_index-1)) )
 		{
-			/*
-			if(radar_checkBox_show_ir_north->Checked)		update_grid(i,IR_N,Convert::ToSingle(radar_txt_calib_n_x->Text), Convert::ToSingle(radar_txt_calib_n_y->Text), Convert::ToSingle(radar_txt_calib_n_theta->Text) * (PI/180.0f));
-			//if(radar_checkBox_show_ir_far_north->Checked)
-			if(radar_checkBox_show_ir_nw->Checked)			update_grid(i,IR_NW,Convert::ToSingle(radar_txt_calib_nw_x->Text),Convert::ToSingle(radar_txt_calib_nw_y->Text),Convert::ToSingle(radar_txt_calib_nw_theta->Text) * (PI/180.0f));
-			if(radar_checkBox_show_ir_ne->Checked)			update_grid(i,IR_NE,Convert::ToSingle(radar_txt_calib_ne_x->Text),Convert::ToSingle(radar_txt_calib_ne_y->Text),Convert::ToSingle(radar_txt_calib_ne_theta->Text) * (PI/180.0f));
-
-			if(radar_checkBox_show_ir_4->Checked)			update_grid(i,IR_E,Convert::ToSingle(radar_txt_calib_4_x->Text),Convert::ToSingle(radar_txt_calib_4_y->Text),Convert::ToSingle(radar_txt_calib_4_theta->Text) * (PI/180.0f));
-			if(radar_checkBox_show_ir_5->Checked)			update_grid(i,IR_SE,Convert::ToSingle(radar_txt_calib_5_x->Text),Convert::ToSingle(radar_txt_calib_5_y->Text),Convert::ToSingle(radar_txt_calib_5_theta->Text) * (PI/180.0f));
-			if(radar_checkBox_show_ir_6->Checked)			update_grid(i,IR_SW,Convert::ToSingle(radar_txt_calib_6_x->Text),Convert::ToSingle(radar_txt_calib_6_y->Text),Convert::ToSingle(radar_txt_calib_6_theta->Text) * (PI/180.0f));
-			if(radar_checkBox_show_ir_7->Checked)			update_grid(i,IR_W,Convert::ToSingle(radar_txt_calib_7_x->Text),Convert::ToSingle(radar_txt_calib_7_y->Text),Convert::ToSingle(radar_txt_calib_7_theta->Text) * (PI/180.0f));
-			//g->FillEllipse(b2, (int)rx, (int)ry ,pensize, pensize);
-			*/
 			{
-				int j;
-				t_inputs h,h2;
 				h = inputs_history[i+0];
-<<<<<<< HEAD
-				h2 = inputs_history[i + 1]; //need +1 here for motion comp
-=======
 				h2= inputs_history[i+1]; //need +1 here for motion comp
 				h.lidar.angle*=4;
 				h2.lidar.angle*=4;
-				log_printf("numsamples=%d  angle=%d,  s1=%d\n",h2.lidar.num_samples,  h2.lidar.angle, h2.lidar.samples[0]);
->>>>>>> origin/master
+				//log_printf("numsamples=%d  angle=%d,  s1=%d\n",h2.lidar.num_samples,  h2.lidar.angle, h2.lidar.samples[0]);
+
+				if(radar_CheckBox_immediate_update->Checked)
+				{
+					h.x = (grid_size*cell_size)/2; //  1000;
+					h.y = (grid_size*cell_size)/2; //  1000;
+					h.theta = 0;
+				}
 
 				for(j=0;j<h2.lidar.num_samples;j++)
 				{
 #if IMMEDIATE_UPDATE
-					update_grid(h2.lidar.samples[j]/* *1000 */, h.x, h.y, h.theta, 0,0,(h2.lidar.angle+j+90) * (PI/180.0f));
+					update_grid(h2.lidar.samples[j]/* *1000 */, h.x, h.y, h.theta, 0,0,(h2.lidar.angle+j+angle) * (PI/180.0f));
 #else
-					update_grid(h2.lidar.samples[j]*1000, (grid_size*cell_size)/2 + h.x-x, (grid_size*cell_size)/2 + h.y-y, h.theta - theta, 0,0,(h2.lidar.angle+j+90) * (PI/180.0f));
+					update_grid(h2.lidar.samples[j]/* *1000 */, (grid_size*cell_size)/2 + h.x-x, (grid_size*cell_size)/2 + h.y-y, h.theta - theta, 0,0,(h2.lidar.angle+j+angle) * (PI/180.0f));
 					if(((int)(h2.lidar.angle+j))==180)
 					{
 						int increment = (g->VisibleClipBounds.Height / grid_size);
-						int ex = (int)((((grid_size*cell_size) / 2) - 100) / cell_size)*increment;
-						int ey = (int)g->VisibleClipBounds.Height - (int)((((grid_size*cell_size) / 2) + 100) / cell_size)*(increment);
+						int ex = (int)((((grid_size*cell_size) / 2) /*- 100*/ ) / cell_size)*increment;
+						int ey = (int)g->VisibleClipBounds.Height - (int)((((grid_size*cell_size) / 2) /*+ 100*/) / cell_size)*(increment);
 						t2 = timeGetTime();
 						ShowRadar(ex, ey);
 						t3 = timeGetTime(); //t3-t2 is 160ms
@@ -335,18 +329,39 @@ namespace robot_ui
 			i++;
 		}
 
-		t2 = timeGetTime();
 #if IMMEDIATE_UPDATE
 		{
-			t_inputs h = inputs_history[i + 0];
-			int increment = (g->VisibleClipBounds.Height / grid_size);
-			int ex = (int)((h.x - 0) / cell_size)*increment;
-			int ey = (int)g->VisibleClipBounds.Height - (int)((h.y + 0) / cell_size)*(increment);
+			h = inputs_history[i + 0];
+			if(radar_CheckBox_immediate_update->Checked)
+			{
+				h.x = (grid_size*cell_size)/2; //  1000;
+				h.y = (grid_size*cell_size)/2; //  1000;
+				h.theta = 0;
+			}
+			float increment = (g->VisibleClipBounds.Height / grid_size);
+			int ex = (int)(((h.x - 0) / cell_size)*increment);
+			int ey = (int)(g->VisibleClipBounds.Height - ((int)((h.y + 0) / cell_size))*(increment));
 			ShowRadar(ex, ey);
 		}
 #endif
+
+	if(radar_checkBox_grid->Checked) DrawGrid(g);
+
+	t2 = timeGetTime();
+
+	count++;
+	if(timeGetTime() - t3 > 1000)
+	{
+		t3 = timeGetTime();
+		log_printf("%d grid updates per second\n",count);
+		count=0;
+	}
+
 #endif //#ifdef USE_LIDAR
 	}
+
+
+
 
 	System::Void f1::radar_timer_Tick(System::Object^  sender, System::EventArgs^  e) 
 	{
@@ -365,29 +380,10 @@ namespace robot_ui
 		static int i=0;
 		float theta;
 		float x,y;
-
 		//log("radar_tabPage_Paint\n");
-
-#if 0
-
-		if(history_index - i > 2000) i=history_index-2000;
-
-		for(i=history_index-360; i<=history_index;i++)
-		{
-			if(i>=0)
-			{
-				theta = inputs_history[i].theta;
-				log_printf("radar_tabPage_Paint(): theta = %04.1f\n",theta);
-				y=310.0f + 300.0f * sinf( theta * 3.14f / 180.0f);
-				x=310.0f + 300.0f * cosf( theta * 3.14f / 180.0f);
-				//g->FillEllipse(b2,(int) x, (int) y, 4, 4);
-				g->DrawEllipse(p,(int) x, (int) y, 4, 4);
-			}
-			//i++;
-		}
-		//g->FillEllipse(b3,(int) x, (int) y, 3, 3);
-#endif
 	}
+
+
 
 	System::Void f1::radar_btn_Clear(System::Object^  sender, System::EventArgs^  e) 
 	{
@@ -405,6 +401,5 @@ namespace robot_ui
 		pictureBox1->Invalidate();
 		g = pictureBox1->CreateGraphics();
 		log_printf("g-> X,Y,Width,Height = %f,%f,%f,%f\n",g->VisibleClipBounds.X,g->VisibleClipBounds.Y,g->VisibleClipBounds.Width,g->VisibleClipBounds.Height);
-
 	}
 }
